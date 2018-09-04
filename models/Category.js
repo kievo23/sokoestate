e2e6dc9f43a1fad47bdeeaa07293824d50d13b5c
kev <kelvinchege@gmail.com>
2018-09-04 13:05:20 +0300

    soko estate in categories

---
+++ models/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.plugin(schema => { schema.options.usePushEach = true });
+var sys = require(__dirname + '/../config/System');
+
+var db = mongoose.connect(sys.db_uri, {useMongoClient: true });
+mongoose.Promise =require('bluebird');
+
+const Schema = mongoose.Schema;
+
+const catSchema = new Schema({
+		name: { type: String,required: true, index: { unique: true, sparse: true }},
+		slug: {
+			type: String,
+			unique: true
+		},
+		icon: String,
+		approved: String,
+		order: String,
+		group: String,
+		photo: String,
+		subcategories: [{
+			name: {type: String, index: { unique: true }}
+		}],
+});
+
+module.exports = mongoose.model('Category', catSchema);
