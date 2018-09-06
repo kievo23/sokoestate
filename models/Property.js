10a159d7be223f53e019d2710ecae4e8ae7a8f14
kev <kelvinchege@gmail.com>
2018-09-06 16:06:30 +0300

    transaction id records

---
+++ models/Property.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+mongoose.plugin(schema => { schema.options.usePushEach = true });
+var sys = require(__dirname + '/../config/System');
+
+var db = mongoose.connect(sys.db_uri, {useMongoClient: true });
+mongoose.Promise =require('bluebird');
+
+const Schema = mongoose.Schema;
+
+const propertySchema = new Schema({
+		name: { type: String,required: true, index: { unique: true, sparse: true }},
+		slug: {
+			type: String,
+			unique: true
+		},
+		category: String,
+    type: String,
+    contact: String,
+    photo: String,
+    gallery: Array,
+    surburb : String,
+    map: {
+			lati: String,
+			long: String,
+			zoom: String
+		},
+    description: String,
+    amenities: Array,
+		approved: String,
+});
+
+module.exports = mongoose.model('Property', propertySchema);
--- routes/property.js
+++ routes/property.js
@@ -8,6 +8,7 @@ var moment = require('moment');
 var Jimp = require("jimp");
 
 var Category = require(__dirname + '/../models/Category');
+var Property = require(__dirname + '/../models/Property');
 //var Subcategory = require(__dirname + '/../models/Subcategory');
 
 
@@ -26,6 +27,38 @@ var cpUpload = upload.fields([{ name: 'photo', maxCount: 1 }, { name: 'catalog',
 
 router.get('/add', function(req, res, next){
 	res.render('property/new');
+});
+
+router.post('/add', cpUpload, function(req, res, next){
+  var i = new Property();
+	i.name = req.body.name;
+  i.slug = slug(req.body.name);
+	i.icon = req.body.icon;
+	i.order = req.body.order;
+  if (req.files['photo'] != null){
+		i.photo = req.files['photo'][0].filename;
+	}
+	i.save(function(err){
+		if(err){
+      console.log(err);
+      req.flash("error_msg", "Category Failed");
+      res.render('property/add');
+    }else{
+      if (req.files['photo'] != null){
+  				Jimp.read("./public/uploads/property/"+i.photo).then(function (cover) {
+  				    return cover.resize(200, 150)     // resize
+  				         .quality(100)              // set greyscale
+  				         .write("./public/uploads/thumbs/properties/"+i.photo); // save
+  				}).catch(function (err) {
+  				    console.error(err);
+  				});
+  			}else{
+
+  			}
+      req.flash("success_msg", "Property Successfully Created");
+  		res.redirect('/property');
+    }
+	});
 });
 
 module.exports = router;
