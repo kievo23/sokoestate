f0d08d585244fae2ff1f34b5372b669fce7532b9
dlelo <akinyidonattah@gmail.com>
2018-09-07 15:41:55 +0300

    add single  product page style

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,6 +3,7 @@ $(window).load(function () { // makes sure the whole site is loaded
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(350).css({'overflow': 'visible'});
 })
+
 $(document).ready(function () {
 
     
