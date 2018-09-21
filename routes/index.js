b570bee9a1a376afaaa68cae9fe96b08f5d9e8c1
kev <kelvinchege@gmail.com>
2018-09-21 16:06:10 +0300

    homepage edits

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Property = require(__dirname + '/../models/Property');
+var Category = require(__dirname + '/../models/Category');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Category.find({})
+  .then(function(data){
+  	//console.log(data);
+    res.render('index', {title: "Soko Estate", categories: data});
+  })
+  .catch(function(err){
+     console.log(err);
+  });
 });
 
 router.get('/login', function(req, res, next) {
@@ -12,6 +21,16 @@ router.get('/login', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('site/register', { title: 'Soko Estate: Register' });
+});
+
+router.get('/search', function(req, res, next) {
+  Property.find({
+    type: req.query.type,
+    category: req.query.category
+  }).then(function(d){
+
+  })
+  res.render('property/indexfront', { title: 'Soko Estate: Register' });
 });
 
 module.exports = router;
