42f067bc86a6e16ab18b8a9f1ea67d93a4a90f39
kev <kelvinchege@gmail.com>
2018-09-26 16:12:16 +0300

    google maps

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: config.google.clientId,
     clientSecret: config.google.clientSecret,
-    callbackURL: "http://sokoestate.com/auth/google/callback",
+    callbackURL: "https://sokoestate.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
@@ -131,7 +131,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: config.facebook.clientId,
     clientSecret: config.facebook.clientSecret,
-    callbackURL: "http://sokoestate.com/auth/facebook/callback"
+    callbackURL: "https://sokoestate.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({ facebookid: profile.id }, function (err, user) {
