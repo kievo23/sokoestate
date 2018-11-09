1c485b5d05a7c47fd5a4e9405e6d211efcc714c3
kev <kelvinchege@gmail.com>
2018-11-09 14:54:03 +0300

    fixes

--- routes/user.js
+++ routes/user.js
@@ -50,4 +50,71 @@ router.get('/delete/:id',role.admin, function(req, res, next){
 		});
 });
 
+
+router.get('/changepassword', role.auth, function(req, res){
+	res.render('user/changepassword');
+});
+
+router.post('/changepassword', role.auth, function(req, res){
+	Users.findById(res.locals.user.id)
+	.then(function(data){
+  		console.log(data);
+  		if(!bcrypt.compareSync(req.body.oldpassword, data.password)){
+  			console.log("wrong password");
+  			req.flash("error_msg","Wrong Current Password");
+  			res.render("user/changepassword");
+  		}else {
+  			if(req.body.newpassword == req.body.confirmpassword){
+  				var salt = bcrypt.genSaltSync(10);
+      			var hash = bcrypt.hashSync(req.body.newpassword, salt);
+      			data.password = hash;
+      			console.log("new password match");
+      			data.save(function(err){
+      				if(err){
+      					console.log("saving error");
+      					req.flash("error_msg","Error Occured");
+  						res.render("user/changepassword");
+      				}else{
+      					console.log("new password saved");
+      					req.flash("success_msg","Password Successfully Changed");
+  						res.redirect("/");
+      				}
+
+      			});
+  			}else{
+  				console.log("new password dont match");
+  				req.flash("error_msg","Password Mismatch");
+  				res.render("user/changepassword");
+  			}
+  		}
+	})
+	.catch(function(err){
+	    console.log(err);
+	});
+});
+
+router.get('/profile', function(req, res){
+  res.render('user/profile', {title: "Profile"});
+});
+
+router.get('/editprofile', function(req, res){
+  res.render('user/editprofile', {title: "Profile"});
+});
+
+router.post('/editprofile', function(req, res){
+  User.findById(res.locals.user._id)
+  .then(function(b){
+    b.names = req.body.names;
+    b.phone = req.body.phone;
+    b.save(function(err){
+      if(err){
+        res.redirect('/editprofile');
+      }else{
+        req.flash('success_msg','Profile Updated');
+        res.redirect('/dashboard');
+      }
+    });
+  });
+});
+
 module.exports = router;
