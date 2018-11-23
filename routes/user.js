0a2e8e799039dfbbe84239da941e0508322e4529
kev <kelvinchege@gmail.com>
2018-11-23 13:05:43 +0300

    users

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ var role = require(__dirname + '/../config/Role');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/')
+    cb(null, './public/uploads/users/')
   },
   filename: function (req, file, cb) {
     var fileName = Date.now() + slug(file.originalname) +'.'+ mime.extension(file.mimetype);
